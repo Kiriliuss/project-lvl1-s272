260c78319d2326e682d8d3857f5417e2711bc2fb
KiRiliUSS <KiRiliUSS@MAIL.RU>
2018-05-12 10:25:03 +0300

    refactoring

--- src/games/balance.js
+++ src/games/balance.js
@@ -3,16 +3,17 @@ import run from '../process';
 import randomNumber from '../utils';
 
 const task = 'Balance the given number.';
-const balance = (array) => {
-  const sortArray = array.sort((a, b) => a - b);
-  const minNumber = sortArray[0];
-  const maxNumber = sortArray[sortArray.length - 1];
-  if (maxNumber - minNumber <= 1) {
-    return sortArray.join('');
+const minNumber = array => Math.min.apply(null, array);
+const maxNumber = array => Math.max.apply(null, array);
+const balance = (newArray) => {
+  const numArray = newArray;
+  while ((maxNumber(numArray) - minNumber(numArray)) > 1) {
+    const minPos = numArray.indexOf(minNumber(numArray));
+    const maxPos = numArray.indexOf(maxNumber(numArray));
+    numArray[minPos] += 1;
+    numArray[maxPos] -= 1;
   }
-  sortArray[0] += 1;
-  sortArray[sortArray.length - 1] -= 1;
-  return balance(sortArray);
+  return numArray.sort((a, b) => a - b).join('');
 };
 const numberToNumArray = number => number.split('').map(Number);
 const gameData = () => {
