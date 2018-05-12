afb80d9adf779b959a4b5e27e3668116d8d016a1
KiRiliUSS <KiRiliUSS@MAIL.RU>
2018-05-12 12:30:34 +0300

    refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -6,7 +6,7 @@ const task = 'Answer "yes" if number even otherwise answer "no"';
 const isEven = number => ((number % 2) === 0);
 const gameData = () => {
   const question = `${randomNumber(0, 50)}`;
-  const trueAnswer = isEven(question) ? 'yes' : 'no';
+  const trueAnswer = isEven(Number(question)) ? 'yes' : 'no';
   return [question, trueAnswer];
 };
 
